de151cb43a4f7d05d46a0422384d528d25795c20
sinta <ramenrice555@gmail.com>
2025-11-20 22:15:18 +0900

    APIテスト用のtestAPI.pyを追加

---
+++ test/unit_test/testAPI.py
@@ -0,0 +1,25 @@
+import pytest
+import requests
+
+endpoint = "http://127.0.0.1:8000"
+
+#HTTP ヘッダーリクエスト
+header = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+def test01():
+    
+    #送信するJSONデータ
+    data = {
+        "id" : "12345"
+    }
+        
+    
+    
+    response = requests.post(
+        endpoint, headers=header, data=json.dump(data)
+    )
+    
+    assert response.status_code == 200
